604455f5d9fe9477d5c3c403d6c9b83c4229f909
Bo-Zhang-819 <56896198+Bo-Zhang-819@users.noreply.github.com>
2020-10-27 11:03:40 -0700

    push for record

--- src/app/appointments/appointments.component.spec.ts
+++ src/app/appointments/appointments.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ApptTablesComponent } from './appt-tables.component';
+import { AppointmentsComponent } from './appointments.component';
 
-describe('ApptTablesComponent', () => {
-  let component: ApptTablesComponent;
-  let fixture: ComponentFixture<ApptTablesComponent>;
+describe('AppointmentsComponent', () => {
+  let component: AppointmentsComponent;
+  let fixture: ComponentFixture<AppointmentsComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ApptTablesComponent ]
+      declarations: [ AppointmentsComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(ApptTablesComponent);
+    fixture = TestBed.createComponent(AppointmentsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
--- src/app/appt-records/appt-records.component.spec.ts
+++ src/app/appt-records/appt-records.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ApptTablesComponent } from './appt-tables.component';
+import { ApptRecordsComponent } from './appt-records.component';
 
-describe('ApptTablesComponent', () => {
-  let component: ApptTablesComponent;
-  let fixture: ComponentFixture<ApptTablesComponent>;
+describe('ApptRecordsComponent', () => {
+  let component: ApptRecordsComponent;
+  let fixture: ComponentFixture<ApptRecordsComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ApptTablesComponent ]
+      declarations: [ ApptRecordsComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(ApptTablesComponent);
+    fixture = TestBed.createComponent(ApptRecordsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
