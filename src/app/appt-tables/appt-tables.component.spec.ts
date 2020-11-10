ce27d317d115fdd84e0c76c80739c6ca2108f541
caemanswartz <caeman@pdx.edu>
2020-11-09 17:04:17 -0800

    fixed some tests

--- src/app/appt-tables/appt-tables.component.spec.ts
+++ src/app/appt-tables/appt-tables.component.spec.ts
@@ -10,177 +10,6 @@ describe('ApptTablesComponent', () => {
 
   // test data resembles api data but with only the data appt-tables needs
   // this data will help keep tests consistent
-  const testData = [
-    {
-      id: '0',
-      condition: 'Scheduled',
-      contactName: 'Sam Student0',
-      contactId: '99',
-      interactionOwnerName: 'Adam Advisor',
-      interactionId: '10',
-      ownerName: 'Adam Advisor',
-      subject: 'graduation',
-      teamName: 'Red Team',
-      name: 'Sam0',
-      time: '10:00 AM - 10:50 AM'
-    },
-    {
-      id: '1',
-      condition: 'Checked In',
-      contactName: 'Sam Student1',
-      contactId: '100',
-      interactionOwnerName: 'Adam Advisor',
-      interactionId: '20',
-      ownerName: 'Adam Advisor',
-      subject: 'graduation',
-      teamName: 'Red Team',
-      name: 'Sam1',
-      time: '10:00 AM - 10:50 AM'
-    },
-    {
-      id: '2',
-      condition: 'Active',
-      contactName: 'Sam Student2',
-      contactId: '101',
-      interactionOwnerName: 'Adam Advisor',
-      interactionId: '30',
-      ownerName: 'Adam Advisor',
-      subject: 'graduation',
-      teamName: 'Red Team',
-      name: 'sam2',
-      time: '10:00 AM - 10:50 AM'
-    },
-    {
-      id: '3',
-      condition: 'Canceled',
-      contactName: 'Sam Student3',
-      contactId: '102',
-      interactionOwnerName: 'Adam Advisor',
-      interactionId: '40',
-      ownerName: 'Adam Advisor',
-      subject: 'graduation',
-      teamName: 'Red Team',
-      name: 'Sam3',
-      time: '10:00 AM - 10:50 AM'
-    },
-    {
-      id: '4',
-      condition: 'Finished',
-      contactName: 'Sam Student4',
-      contactId: '103',
-      interactionOwnerName: 'Adam Advisor',
-      interactionId: '50',
-      ownerName: 'Adam Advisor',
-      subject: 'graduation',
-      teamName: 'Red Team',
-      name: 'Sam4',
-      time: '10:00 AM - 10:50 AM'
-    },
-    {
-      id: '5',
-      condition: 'No-show',
-      contactName: 'Sam Student5',
-      contactId: '104',
-      interactionOwnerName: 'Adam Advisor',
-      interactionId: '60',
-      ownerName: 'Adam Advisor',
-      subject: 'graduation',
-      teamName: 'Red Team',
-      name: 'Sam5',
-      time: '10:00 AM - 10:50 AM'
-    },
-    {
-      id: '6',
-      condition: '(Test)',
-      contactName: 'Sam Student6',
-      contactId: '105',
-      interactionOwnerName: 'Adam Advisor',
-      interactionId: '70',
-      ownerName: 'Adam Advisor',
-      subject: 'graduation',
-      teamName: 'Red Team',
-      name: 'Sam6',
-      time: '10:00 AM - 10:50'
-    },
-    {
-      id: '7',
-      condition: 'Finished',
-      contactName: 'Sam Student7',
-      contactId: '106',
-      interactionOwnerName: 'Adam Advisor',
-      interactionId: '80',
-      ownerName: 'Adam Advisor',
-      subject: 'graduation',
-      teamName: 'Red Team',
-      name: 'Sam7',
-      time: '10:00 AM - 10:50 AM'
-    },
-    {
-      id: '8',
-      condition: 'Finished',
-      contactName: 'Sam Student8',
-      contactId: '107',
-      interactionOwnerName: 'Adam Advisor',
-      interactionId: '90',
-      ownerName: 'Adam Advisor',
-      subject: 'graduation',
-      teamName: 'Red Team',
-      name: 'Sam8',
-      time: '10:00 AM - 10:50 AM'
-    },
-    {
-      id: '9',
-      condition: 'Finished',
-      contactName: 'Sam Student9',
-      contactId: '108',
-      interactionOwnerName: 'Adam Advisor',
-      interactionId: '100',
-      ownerName: 'Adam Advisor',
-      subject: 'graduation',
-      teamName: 'Red Team',
-      name: 'Sam9',
-      time: '10:00 AM - 10:50 AM'
-    },
-    {
-      id: '10',
-      condition: 'Finished',
-      contactName: 'Sam Student10',
-      contactId: '109',
-      interactionOwnerName: 'Adam Advisor',
-      interactionId: '110',
-      ownerName: 'Adam Advisor',
-      subject: 'graduation',
-      teamName: 'Red Team',
-      name: 'Sam10',
-      time: '10:00 AM - 10:50 AM'
-    },
-    {
-      id: '11',
-      condition: 'Finished',
-      contactName: 'Sam Student11',
-      contactId: '110',
-      interactionOwnerName: 'Adam Advisor',
-      interactionId: '120',
-      ownerName: 'Adam Advisor',
-      subject: 'graduation',
-      teamName: 'Red Team',
-      name: 'Sam11',
-      time: '10:00 AM - 10:50 AM'
-    },
-    {
-      id: '12',
-      condition: 'No-show',
-      contactName: 'Sam Student12',
-      contactId: '111',
-      interactionOwnerName: 'Adam Advisor',
-      interactionId: '130',
-      ownerName: 'Adam Advisor',
-      subject: 'graduation',
-      teamName: 'Red Team',
-      name: 'Sam12',
-      time: '10:00 AM - 10:50 AM'
-    }
-  ];
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -211,13 +40,13 @@ describe('ApptTablesComponent', () => {
   });
   it('apply matching any filter', () => {
     // apply filter
-    component.filterME({ data: 'abc' }, 2, 'masters program');
+    component.filterMA({ data: 'abc' }, 2, 'masters program');
     // check a name is there
     expect(component.tables[2].source).toContain(jasmine.objectContaining({subject: 'masters program'}));
     // check a similar name is
     expect(component.tables[2].source).toContain(jasmine.objectContaining({subject: 'postbacc program'}));
     // check a different name isn't
-    expect(component.tables[2].source).not.toEqual(jasmine.objectContaining('graduation'));
+    expect(component.tables[2].source).not.toEqual(jasmine.objectContaining({subject: 'graduation'}));
     component.initializeAllArrays();
     fixture.detectChanges();
   });
