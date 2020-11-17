b30aa2c94a1d85ad983725a2899871c114114b27
Bo-Zhang-819 <56896198+Bo-Zhang-819@users.noreply.github.com>
2020-11-16 17:02:30 -0800

    the footer of record page updates

--- src/app/appt-records/appt-records.component.ts
+++ src/app/appt-records/appt-records.component.ts
@@ -12,6 +12,7 @@ export class ApptRecordsComponent implements OnInit {
 
   isPhoto = false;
   recordData;
+  createInformation;
 
   constructor(private apiService: ApiService) { }
 
@@ -23,8 +24,44 @@ export class ApptRecordsComponent implements OnInit {
   closeRecordView(): void {
     this.toggleRecord.emit();
   }
+  
+  createInfo(info: any){
+    this.createInformation = info;
+    console.log(this.createInformation);
+    let editTable = document.getElementById("tbody");
+    let tr = document.createElement("tr");
+    let td0 = document.createElement("td");
+    td0.innerHTML= this.createInformation.subject;
+    let td1 = document.createElement("td");
+    td1.innerHTML =this.recordData.teamName;
+    let td2 =document.createElement("td");
+    td2.innerHTML = this.recordData.ownerName;
+    let td3 =document.createElement("td");
+    td3.innerHTML= this.createInformation.medium;
+    let td4 =document.createElement("td");
+    td4.innerHTML = "Pending";
+    let td5 =document.createElement("td");
+    var myDate = new Date();
+    td5.innerHTML = myDate.toLocaleString();
+    let td6 =document.createElement("td");
+    tr.appendChild(td0);
+	  tr.appendChild(td1);
+	  tr.appendChild(td2);
+    tr.appendChild(td3);
+    tr.appendChild(td4);
+	  tr.appendChild(td5);
+    tr.appendChild(td6);
+    editTable.appendChild(tr);
+    alert("Create Successfully!");
+  }
 
   dropMenu(): void {
     document.getElementById('myDropdown').classList.toggle('show');
   }
+
+  
+  
+
+
+  
 }
